7b525330cbd8ea7644c202f7c3baf6e0e7e298f2
Ice713 <icramzilruiz@gmail.com>
2025-09-20 12:23:57 +0800

    Created Student Class with getter, setter, and constructor.
    GPA also has validation, if < 0 or > 4.0, set to 0.0.

--- Case4StudentGradeManagement/Student.cs
+++ Case4StudentGradeManagement/Student.cs
@@ -12,7 +12,32 @@ namespace Case4StudentGradeManagement
         private string course;
         private double gpa;
 
-        public 
+        public string Name
+        { 
+            get { return name; } 
+            set { name = value; } 
+        }
+
+        public string Course
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Gpa
+        {
+            get { return gpa; }
+            set
+            {
+                if (value < 0 || value > 4.0)
+                {
+                    gpa = 0;
+                    throw new ArgumentOutOfRangeException("Invalid GPA! Setting GPA to 0.0");
+                }
+                else
+                    gpa = value;
+            }
+        }
 
     }
 }
