0e166772145517e15983dd5b63ac0a55c49aadd3
Ice713 <icramzilruiz@gmail.com>
2025-09-20 10:40:30 +0800

    Add project files.

---
+++ Case4StudentGradeManagement/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case4StudentGradeManagement
+{
+    internal class Student
+    {
+        private string name;
+        private string course;
+        private double gpa;
+
+        public 
+
+    }
+}
